56a5b8720a083cd937a625c0a3525764387d6915
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2025-03-02 03:27:51 +0530

    created navbar

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <>
+      <nav className="absolute top-0 left-0 w-full px-[13%] py-4 border-b border-b-gray-300 flex justify-between items-center">
+        <div className="font-mono text-lg tracking-wide">
+          <Link href="/">Nadeem M Siyam</Link>
+        </div>
+        <div className="text-gray-600 font-mono flex gap-4">
+          <Link href="">About</Link>
+          <Link href="">Projects</Link>
+          <Link href="">Services</Link>
+          <Link href="">Skills</Link>
+          <Link href="">Contact</Link>
+        </div>
+      </nav>
+    </>
+  );
+}
