3f901c9d00a0bad1065593271d7ffe51072342d3
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2025-03-02 16:08:08 +0530

    fixed navbar and completed hero sectionm

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,28 @@
+import { Bricolage_Grotesque } from "next/font/google";
+
+const bric = Bricolage_Grotesque({ subsets: ["latin"] });
+
 export default function Home() {
   return (
     <>
-      <main></main>
+      <main className="min-h-screen flex justify-center bg-gradient-to-b from-[#e6eee3] to-[var(--accent)]">
+        <div className="mt-32 flex flex-col items-center">
+          <div
+            className={`${bric.className} text-[14vw] md:text-[8vw] lg:text-[6vw] font-bold text-[var(--primary-color)]`}
+          >
+            <h1>I'm Nadeem.</h1>
+            <h2 className="-mt-[7%]">A Developer.</h2>
+          </div>
+          <div className="mx-4 sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl">
+            <p className="text-center text-[#4b5f5a] font-medium text-[3.5vw] md:text-[2vw] lg:text-[1.5vw] xl:text-[1.05vw]">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum eos
+              amet corporis quas pariatur? Mollitia?
+            </p>
+          </div>
+        </div>
+      </main>
+      <section id="" className="h-screen"></section>
+      <section id="" className="h-screen"></section>
     </>
   );
 }
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,15 +3,14 @@ import Link from "next/link";
 export default function Navbar() {
   return (
     <>
-      <nav className="z-10 absolute top-0 left-0 w-full px-[13%] py-4 border-b border-b-gray-300 flex justify-between items-center">
-        <div className="font-mono text-lg tracking-wide">
+      <nav className="z-10 absolute top-0 left-0 w-full px-[3%] md:px-[13%] py-4 border-b border-b-gray-300 flex justify-between items-center backdrop-blur-[5px]">
+        <div className="font-mono md:text-lg">
           <Link href="/">Nadeem M Siyam</Link>
         </div>
-        <div className="text-gray-600 font-mono flex gap-4">
+        <div className="hidden text-gray-600 font-mono md:flex gap-4">
           <Link href="">About</Link>
           <Link href="">Projects</Link>
-          <Link href="">Services</Link>
-          <Link href="">Skills</Link>
+          <Link href="">Play</Link>
           <Link href="">Contact</Link>
         </div>
       </nav>
