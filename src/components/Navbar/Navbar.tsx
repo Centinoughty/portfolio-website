9e613863557c9c4f47d87739fa8a986230dacd55
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2025-03-02 11:26:24 +0530

    updated navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Navbar() {
   return (
     <>
-      <nav className="absolute top-0 left-0 w-full px-[13%] py-4 border-b border-b-gray-300 flex justify-between items-center">
+      <nav className="z-10 absolute top-0 left-0 w-full px-[13%] py-4 border-b border-b-gray-300 flex justify-between items-center">
         <div className="font-mono text-lg tracking-wide">
           <Link href="/">Nadeem M Siyam</Link>
         </div>
