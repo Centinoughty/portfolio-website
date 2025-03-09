26174aa60cbca9bf9a2c2fb80fe329fdefd45bb5
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2025-03-09 12:57:05 +0530

    created copy hook and connect card

--- src/components/Card/ConnectCard.tsx
+++ src/components/Card/ConnectCard.tsx
@@ -1,7 +1,21 @@
+import Link from "next/link";
+import { EMAIl } from "../../../util/constant";
+import { bric } from "@/styles/fonts";
+
 export default function ConnectCard() {
   return (
     <>
-      <div></div>
+      <div>
+        <div className={`${bric.className} text-[1.2vw]`}>
+          <p>Send me a mail at</p>
+          <Link
+            href={`mailto:${EMAIl}`}
+            className="underline font-semibold tracking-wide"
+          >
+            {EMAIl}
+          </Link>
+        </div>
+      </div>
     </>
   );
 }
--- src/components/Card/ContactCard.tsx
+++ src/components/Card/ContactCard.tsx
@@ -17,7 +17,7 @@ export default function ContactCard() {
     <>
       <form
         onSubmit={handleSubmit}
-        className="w-screen md:max-w-md lg:max-w-lg px-6 py-4 shadow-md rounded-md flex flex-col items-center gap-4"
+        className="w-screen md:max-w-md lg:max-w-lg p-4 shadow-md rounded-md flex flex-col items-center gap-4"
       >
         <Input title="Name" type="text" />
         <Input title="Email" type="email" />
