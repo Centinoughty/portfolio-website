ceadd2ffb22b15fc9c58c65589aa8571b7938a79
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2025-03-12 01:15:59 +0530

    created projects page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,7 +1,17 @@
+import { bric } from "@/styles/fonts";
+
 export default function Home() {
   return (
     <>
-      <main></main>
+      <main className="pt-16 mx-[5%]">
+        <h1
+          className={`${bric.className} text-[var(--primary-color)] font-bold text-[8vw] md:text-[5vw] lg:text-[4vw] xl:text-[3vw]`}
+        >
+          My Projects
+        </h1>
+
+        <section></section>
+      </main>
     </>
   );
 }
